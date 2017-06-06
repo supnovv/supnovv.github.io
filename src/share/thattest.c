3085e1a94fc65146e78fbbd956eb8f0ef4a12ea3
supnovv <flw_r@163.com>
2017-06-06 21:25:34 +0800

    20170606

--- src/share/thattest.c
+++ src/share/thattest.c
@@ -112,6 +112,45 @@ const char* string_match(struct stringormap* ormap, const char* s, int len) {
   return ccstringtooshort; /* too short to match */
 }
 
+/* match exactly n times, return >=s or ccstringtooshort */
+const char* string_matchtimes(struct stringormap* ormap, int n, const char* s, int len) {
+  const char* e = 0;
+  int i = 0;
+  if (n <= 0) return s;
+  while (i++ < n) {
+    e = string_match(ormap, s, len);
+    if (e == 0) {
+      return s;
+    }
+    if (e == ccstringtooshort) {
+      return e;
+    }
+    s = e;
+    len -= e - s;
+  }
+  return e;
+}
+
+/* return >=s */
+const char* string_matchrepeat(struct stringormap* ormap, const char* s, int len, int* lasttimematchfailed) {
+  const char* e = 0;
+  const char* prev = s;
+
+  while ((e = string_match(ormap, s, len)) != 0 && e != ccstringtooshort) {
+    prev = e;
+    s = e;
+    len -= e - s;
+  }
+
+  if (e == 0) {
+    if (lasttimematchfailed) *lasttimematchfailed = 1;
+  } else {
+    if (lasttimematchfailed) *lasttimematchfailed = 0;
+  }
+
+  return prev;
+}
+
 const char* ccspaces[] = {
   "\x09", /* \t */
   "\x0B", /* \v */
@@ -142,6 +181,42 @@ const char* ccspaces[] = {
 };
 
 const char* ccnewlines[] = {
+  "\x0A\x0D", /* \n\r */
+  "\x0D\x0A", /* \r\n */
+  "\x0A", /* \r */
+  "\x0D", /* \n */
+  "\xE2\x80\xA8", /* line separator 0x2028 00100000_00101000 -> 1110'0010_10'000000_10'101000 (0xE280A8) */
+  "\xE2\x80\xA9", /* paragraph separator 0x2029 00100000_00101001 */
+  0
+};
+
+const char* ccblanks[] = {
+  "\x09", /* \t */
+  "\x0B", /* \v */
+  "\x0C", /* \f */
+  /* Zs 'Separator, Space' Category - www.fileformat.info/info/unicode/category/Zs/list.htm */
+  "\x20", /* 0x20 space */
+  "\xC2\xA0", /* 0xA0 no-break space */
+  "\xE1\x9A\x80", /* 0x1680 ogham space mark */
+  "\xE2\x80\xAF", /* 0x202F narrow no-break space */
+  "\xE2\x81\x9F", /* 0x205F medium mathematical space */
+  "\xE3\x80\x80", /* 0x3000 ideographic space (chinese blank character) */
+  "\xE2\x80\x80", /* 0x2000 en quad */
+  "\xE2\x80\x81", /* 0x2001 em quad */
+  "\xE2\x80\x82", /* 0x2002 en space */
+  "\xE2\x80\x83", /* 0x2003 em space */
+  "\xE2\x80\x84", /* 0x2004 three-per-em space */
+  "\xE2\x80\x85", /* 0x2005 four-per-em space */
+  "\xE2\x80\x86", /* 0x2006 six-per-em space */
+  "\xE2\x80\x87", /* 0x2007 figure space */
+  "\xE2\x80\x88", /* 0x2008 punctuation space */
+  "\xE2\x80\x89", /* 0x2009 thin space */
+  "\xE2\x80\x8A", /* 0x200A hair space */
+  /* byte order marks */
+  "\xFE\xFF",
+  "\xFF\xFE",
+  "\xEF\xBB\xBF",
+  /* new lines */
   "\x0A\x0D", /* \n\r */
   "\x0D\x0A", /* \r\n */
   "\x0A", /* \r */
