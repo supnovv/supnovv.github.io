095860aa9f381c4efc24abe612d9c91a3aaa323a
supnovv <flw_r@163.com>
2017-10-06 20:36:06 +0800

    Update fileop.c

--- src/lucy/core/fileop.c
+++ src/lucy/core/fileop.c
@@ -1,61 +1,89 @@
 #include "core/fileop.h"
 
-static l_filestream llopenfile(const void* name, const char* mode) {
-  l_filestream fs = {0};
-  if (!name) { l_loge_s("empty name"); return fs; }
-  fs.stream = fopen((const char*)name, mode);
-  if (!fs.stream) {
-    l_loge_1("fopen %s", lserror(errno));
+static l_file
+llopenfile(const void* name, const char* mode)
+{
+  l_file fs = {0};
+  if (!name || !mode) {
+    l_loge_s("invalid argument");
+  } else {
+    if (!(fs.stream = fopen((const char*)name, mode))) {
+      l_loge_1("fopen %s", lserror(errno));
+    }
   }
   return fs;
 }
 
-static void llreopenfile(FILE* file, const void* name, const char* mode) {
-  if (!name) { l_loge_s("empty name"); return; }
+static void
+llreopenfile(FILE* file, const void* name, const char* mode)
+{
+  if (!name || !mode) {
+    l_loge_s("invalid argument");
+    return;
+  }
   if (freopen((const char*)name, mode, file) == 0) {
     l_loge_1("freopen %s", lserror(errno));
   }
 }
 
-l_filestream l_open_read(const void* name) {
+L_EXTERN l_file
+l_file_openRead(const void* name)
+{
   return llopenfile(name, "rb");
 }
 
-l_filestream l_open_write(const void* name) {
+L_EXTERN l_file
+l_file_openWrite(const void* name)
+{
   return llopenfile(name, "wb");
 }
 
-l_filestream l_open_append(const void* name) {
+L_EXTERN l_file
+l_file_openAppend(const void* name)
+{
   return llopenfile(name, "ab");
 }
 
-l_filestream l_open_read_write(const void* name) {
+L_EXTERN l_file
+l_file_openReadWrite(const void* name)
+{
   return llopenfile(name, "rb+");
 }
 
-l_filestream l_open_read_unbuffered(const void* name) {
-  l_filestream fs = llopenfile(name, "rb");
+L_EXTERN l_file
+l_file_openReadUnbuffered(const void* name)
+{
+  l_file fs = llopenfile(name, "rb");
   if (!fs.stream) return fs;
   setbuf((FILE*)fs.stream, 0);
   return fs;
 }
 
-l_filestream l_open_write_unbuffered(const void* name) {
+L_EXTERN l_file
+l_file_openWriteUnbuffered(const void* name)
+{
   l_filestream fs = llopenfile(name, "wb");
   if (!fs.stream) return fs;
   setbuf((FILE*)fs.stream, 0);
   return fs;
 }
 
-l_extern l_filestream l_open_append_unbuffered(const void* name) {
-  l_filestream fs = llopenfile(name, "ab");
+L_EXTERN l_file
+l_file_openAppendUnbuffered(const void* name)
+{
+  l_file fs = llopenfile(name, "ab");
   if (!fs.stream) return fs;
   setbuf((FILE*)fs.stream, 0);
   return fs;
 }
 
-int l_remove_file(const void* name) {
-  if (!name) { l_loge_s("empty name"); return L_STATUS_EINVAL; }
+L_EXTERN int
+l_file_remove(const void* name)
+{
+  if (!name) {
+    l_loge_s("empty name");
+    return L_STATUS_EINVAL;
+  }
   if (remove((const char*)name) != 0) {
     l_loge_1("remove %s", lserror(errno));
     return L_STATUS_ERROR;
@@ -63,7 +91,9 @@ int l_remove_file(const void* name) {
   return 0;
 }
 
-int l_rename_file(const void* from, const void* to) {
+L_EXTERN int
+l_file_rename(const void* from, const void* to)
+{
   /* int rename(const char* oldname, const char* newname);
   Changes the name of the file or directory specified by
   oldname to newname. This is an operation performed directly
@@ -73,7 +103,10 @@ int l_rename_file(const void* from, const void* to) {
   location. If newname names an existing file, the function
   may either fail or override the existing file, depending on
   the specific system and library implementation. */
-  if (!from || !to) { l_loge_s("empty name"); return L_STATUS_EINVAL; }
+  if (!from || !to) {
+    l_loge_s("empty name");
+    return L_STATUS_EINVAL;
+  }
   if (rename((const char*)from, (const char*)to) != 0) {
     l_loge_1("rename %s", lserror(errno));
     return L_STATUS_ERROR;
@@ -81,19 +114,27 @@ int l_rename_file(const void* from, const void* to) {
   return 0;
 }
 
-void l_redirect_stdout(const void* name) {
+L_EXTERN void
+l_redirect_stdout(const void* name)
+{
   llreopenfile(stdout, name, "wb");
 }
 
-void l_redirect_stderr(const void* name) {
+L_EXTERN void
+l_redirect_stderr(const void* name)
+{
   llreopenfile(stderr, name, "wb");
 }
 
-void l_reditect_stdin(const void* name) {
+L_EXTERN void
+l_reditect_stdin(const void* name)
+{
   llreopenfile(stdin, name, "rb");
 }
 
-void l_close_file(l_filestream* self) {
+L_EXTERN void
+l_file_close(l_file* self)
+{
   if (!self->stream) return;
   if (fclose((FILE*)self->stream) != 0) {
     l_loge_1("fclose %s", lserror(errno));
@@ -101,37 +142,51 @@ void l_close_file(l_filestream* self) {
   self->stream = 0;
 }
 
-void l_flush_file(l_filestream* self) {
+L_EXTERN void
+l_file_flush(l_file* self)
+{
   if (fflush((FILE*)self->stream) != 0) {
     l_loge_1("fflush %s", lserror(errno));
   }
 }
 
-void l_rewind_file(l_filestream* self) {
+L_EXTERN void
+l_file_rewind(l_file* self)
+{
   rewind((FILE*)self->stream);
 }
 
-void l_seek_from_begin(l_filestream* self, long offset) {
+L_EXTERN void
+l_file_seekFromBegin(l_file* self, long offset)
+{
   if (fseek((FILE*)self->stream, offset, SEEK_SET) != 0) {
     l_loge_1("fseek SET %s", lserror(errno));
   }
 }
 
-void l_seek_from_curpos(l_filestream* self, long offset) {
+L_EXTERN void
+l_file_seekFromCurPos(l_file* self, long offset)
+{
   if (fseek((FILE*)self->stream, offset, SEEK_CUR) != 0) {
     l_loge_1("fseek CUR %s", lserror(errno));
   }
 }
 
-void l_clear_file_error(l_filestream* self) {
+L_EXTERN void
+l_file_clearerr(l_file* self)
+{
   clearerr((FILE*)self->stream);
 }
 
-l_int l_write_strt_to_file(l_filestream* self, l_strt s) {
+L_EXTERN l_int
+l_file_write(l_file* self, l_strt s)
+{
   return l_write_file(self, s.start, s.end - s.start);
 }
 
-l_int l_write_file(l_filestream* self, const void* s, l_int len) {
+L_EXTERN l_int
+l_file_writeLen(l_file* self, const void* s, l_int len)
+{
   l_int n = 0;
   if (!s || len <= 0 || len > l_max_rdwr_size) {
     l_loge_1("invalid %d", ld(len));
@@ -144,7 +199,9 @@ l_int l_write_file(l_filestream* self, const void* s, l_int len) {
   return n;
 }
 
-l_int l_read_file(l_filestream* self, void* out, l_int len) {
+L_EXTERN l_int
+l_file_read(l_file* self, void* out, l_int len)
+{
   l_int n = 0;
   if (!out || len <= 0 || len > l_max_rdwr_size) {
     l_loge_1("invalid %d", ld(len));
